542f070a2e058e3c49ef4dbe4e78a427920c73fa
gianlucca <gianluccadevigili@gmail.com>
2020-11-19 21:33:16 -0300

    Erases the PageRank.py file and adds the  parse func at utils.py

---
+++ utils.py
@@ -0,0 +1,16 @@
+import re, sys, math, csv, types
+import random as rd
+import networkx as nx
+from collections import defaultdict
+
+def parse(filename, isDirected):
+    # Open the file in read mode
+    file = csv.reader(open(filename, 'r'), delimiter=',')
+    data = [row for row in file]
+    
+    print("Reading data")
+    
+    if isDirected:
+        return parse_directed(data)
+    else:
+        return parse_undirected(data)
