d850525cf35677793c1bf5ac3267ddcb11e97273
gianlucca <gianluccadevigili@gmail.com>
2020-11-19 22:27:13 -0300

    Alter parse to only accept directed graphs
    The original code uses both types of graph, but this schoolwork will only use an undirected graph

--- utils.py
+++ utils.py
@@ -11,13 +11,6 @@ def parse(filename, isDirected):
     
     print("Reading data")
     
-    if isDirected:
-        return parse_directed(data)
-    else:
-        return parse_undirected(data)
-
-
-def parse_undirected(data):
     G = nx.Graph()                                          
     nodes = set([row[0] for row in data])                   # (1)-->
     edges = [(row[0], row[2]) for row in data]              # (2)-->
@@ -33,34 +26,16 @@ def parse_undirected(data):
     # -->(2) Define a list with containing the 
     # -->(3) Define the rank value for each link
     
-
-def parse_directed(data):
-    DG = nx.DiGraph()
-
-    for i, row in enumerate(data):
-
-        node_a = format_key(row[0])
-        node_b = format_key(row[2])
-        val_a = digits(row[1])
-        val_b = digits(row[3])
-
-        DG.add_edge(node_a, node_b)
-        if val_a >= val_b:
-            DG.add_path([node_a, node_b])
-        else:
-            DG.add_path([node_b, node_a])
-
-    return DG
+print(parse("stateborders.csv", 0))
 
 def digits(val):
     return int(re.sub("\D", "", val))
 
-def format_key(key):
-    key = key.strip() 
+def format_key(key): #cleans the key string
+    key = key.strip()
     if key.startswith('"') and key.endswith('"'):
         key = key[1:-1]
-    return key 
-
+    return key
 
 def print_results(f, method, results):
-    print (method)
+    print(method)
